a343ab79e468664d6d021f290a6ee143ae672200
aamir-3s <amir.farooq@jjtestsite.us>
2021-11-21 19:32:36 +0500

    commit

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { APIPrefix } from './Constant/commom';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+        app.enableCors(); 
         app.useGlobalPipes( new ValidationPipe());
         app.setGlobalPrefix(APIPrefix.Version);
         
